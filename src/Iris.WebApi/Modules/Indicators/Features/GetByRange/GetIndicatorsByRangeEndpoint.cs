7c31674dd98ad73703cffb141536175e4e3f9b8a
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2025-11-23 20:34:59 -0300

    feat: retrieves indicators from redis

--- src/Iris.WebApi/Modules/Indicators/Features/GetByRange/GetIndicatorsByRangeEndpoint.cs
+++ src/Iris.WebApi/Modules/Indicators/Features/GetByRange/GetIndicatorsByRangeEndpoint.cs
@@ -1,4 +1,8 @@
+using System.Text.Json;
+
 using Iris.WebApi.Modules.Indicators.Models;
+
+using StackExchange.Redis;
 
 namespace Iris.WebApi.Modules.Indicators.Features.GetByRange;
 
@@ -6,19 +10,30 @@ public static class GetIndicatorsByRangeEndpoint
 {
     public static WebApplication MapGetIndicatorsByRangeEndpoint(this WebApplication app)
     {
-        app.MapGet("/indicators", async ([AsParameters] GetIndicatorsByRangeRequest request) =>
+        app.MapGet("/indicators", async (
+            [AsParameters] GetIndicatorsByRangeRequest request,
+            IConnectionMultiplexer redis) =>
         {
-            await Task.Delay(2);
+            IDatabase db = redis.GetDatabase();
 
-            IEnumerable<Indicator> data =
-            [
-                new(DateOnly.Parse("2023-01-01"), 123.45m),
-                new(DateOnly.Parse("2023-01-02"), 678.90m)
-            ];
+            string key = $"indicator:{request.Code}";
+            long fromScore = long.Parse(request.From.ToString("yyyyMMdd"));
+            long toScore = long.Parse(request.To.ToString("yyyyMMdd"));
+
+            RedisValue[] entries = await db.SortedSetRangeByScoreAsync(key, fromScore, toScore);
+
+            if (entries is null || entries.Length == 0) return Results.NoContent();
+
+            JsonSerializerOptions options = new()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            IEnumerable<Indicator> indicators = entries.Select(e => JsonSerializer.Deserialize<Indicator>((string)e!, options));
 
             GetIndicatorsByRangeResponse response = new(
                 Code: request.Code,
-                Data: data
+                Data: indicators
             );
 
             return Results.Ok(response);
--- src/Iris.WebApi/Program.cs
+++ src/Iris.WebApi/Program.cs
@@ -1,5 +1,12 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<IConnectionMultiplexer>(s =>
+{
+    string connection = builder.Configuration.GetConnectionString("redis")!;
+    return ConnectionMultiplexer.Connect(connection);
+});
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
