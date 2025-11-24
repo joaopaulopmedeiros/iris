ec212a58cf96c6358216bc08a54bbe5c3b9507a8
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2025-11-24 20:39:48 -0300

    test: makes arrange

--- tests/Iris.WebApi.FunctionalTests/Modules/Indicators/Features/GetIndicatorsByRangeEndpointTests.cs
+++ tests/Iris.WebApi.FunctionalTests/Modules/Indicators/Features/GetIndicatorsByRangeEndpointTests.cs
@@ -1,17 +1,26 @@
 using Iris.WebApi.Modules.Indicators.Features.GetByRange;
 
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 
+using StackExchange.Redis;
+
+using System.Globalization;
 using System.Net;
 using System.Net.Http.Json;
 
 namespace Iris.WebApi.FunctionalTests.Modues.Indicators.Features;
 
-public class GetIndicatorsByRangeEndpointTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+public class GetIndicatorsByRangeEndpointTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>, IAsyncDisposable
 {
+    private const string TestKey = "indicator:selic";
+    private readonly List<long> _insertedTimestamps = [];
+
     [Fact]
     public async Task GetIndicatorsByRange_ShouldReturnOk_WhenDataExists()
     {
+        await SeedTestDataAsync();
+
         DateOnly from = DateOnly.FromDateTime(DateTime.Now.AddDays(-10));
         DateOnly to = DateOnly.FromDateTime(DateTime.Now);
 
@@ -27,5 +36,68 @@ public class GetIndicatorsByRangeEndpointTests(WebApplicationFactory<Program> fa
 
         Assert.NotEmpty(content.Data);
         Assert.Equal("selic", content.Code);
+
+        Assert.All(content.Data, indicator =>
+        {
+            Assert.True(indicator.Date >= from);
+            Assert.True(indicator.Date <= to);
+        });
+    }
+
+    private async Task SeedTestDataAsync()
+    {
+        using var scope = factory.Services.CreateScope();
+        IConnectionMultiplexer redis = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
+        IDatabase db = redis.GetDatabase();
+
+        if (await db.KeyExistsAsync(TestKey) == false)
+        {
+            await db.ExecuteAsync("TS.CREATE", TestKey, "RETENTION", 0, "LABELS", "code", "selic");
+        }
+
+        List<(long timestamp, decimal value)> testData = GenerateTestData(15);
+
+        List<object> args = [];
+
+        foreach (var (timestamp, value) in testData)
+        {
+            args.Add(TestKey);
+            args.Add(timestamp);
+            args.Add(value.ToString(CultureInfo.InvariantCulture));
+            _insertedTimestamps.Add(timestamp);
+        }
+
+        if (args.Count > 0)
+        {
+            await db.ExecuteAsync("TS.MADD", args.ToArray());
+        }
+    }
+
+    private static List<(long timestamp, decimal value)> GenerateTestData(int days)
+    {
+        List<(long, decimal)> data = [];
+        Random random = new(42);
+
+        for (int i = days; i >= 0; i--)
+        {
+            DateTime date = DateTime.Now.AddDays(-i).Date;
+            long timestamp = new DateTimeOffset(date).ToUnixTimeMilliseconds();
+            decimal value = 10.0m + (decimal)(random.NextDouble() * 3.0);
+
+            data.Add((timestamp, Math.Round(value, 2)));
+        }
+
+        return data;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_insertedTimestamps.Count > 0)
+        {
+            using var scope = factory.Services.CreateScope();
+            var redis = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
+            var db = redis.GetDatabase();
+            await db.KeyDeleteAsync(TestKey);
+        }
     }
 }
