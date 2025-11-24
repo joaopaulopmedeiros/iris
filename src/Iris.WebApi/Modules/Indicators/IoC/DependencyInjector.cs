38b6d819a25135cead3212cb9f2bd4fd9eb1c9d9
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2025-11-23 22:09:11 -0300

    chore: removes unused parts

--- src/Iris.WebApi/Modules/Indicators/IoC/DependencyInjector.cs
+++ src/Iris.WebApi/Modules/Indicators/IoC/DependencyInjector.cs
@@ -9,14 +9,14 @@ public static class DependencyInjector
 {
     public static IApplicationBuilder UseIndicatorsBackgroundJobs(this IApplicationBuilder app)
     {
-        using var scope = app.ApplicationServices.CreateScope();
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
 
-        var jobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
+        IRecurringJobManager jobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
 
         jobManager.AddOrUpdate<SelicIngestionJob>(
             recurringJobId: "selic-ingestion-job",
             methodCall: job => job.ExecuteAsync(),
-            cronExpression: Cron.Daily(),
+            cronExpression: Cron.Minutely(),
             new RecurringJobOptions()
             {
                 TimeZone = TimeZoneInfo.Local
