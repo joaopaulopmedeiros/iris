bb4de6a24a005ce848d5e9096d1da99dc45e12a0
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2025-12-28 13:57:46 -0300

    refactor: simplifies arrange with seeder

--- tests/Iris.WebApi.FunctionalTests/Modules/Indicators/Features/GetIndicatorsByRangeEndpointTests.cs
+++ tests/Iris.WebApi.FunctionalTests/Modules/Indicators/Features/GetIndicatorsByRangeEndpointTests.cs
@@ -1,7 +1,7 @@
-using System.Globalization;
 using System.Net;
 using System.Net.Http.Json;
 
+using Iris.WebApi.FunctionalTests.Modules.Indicators.Seeders;
 using Iris.WebApi.Modules.Indicators.Features.GetByRange;
 
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -9,17 +9,16 @@ using Microsoft.Extensions.DependencyInjection;
 
 using StackExchange.Redis;
 
-namespace Iris.WebApi.FunctionalTests.Modues.Indicators.Features;
+namespace Iris.WebApi.FunctionalTests.Modules.Indicators.Features;
 
 public class GetIndicatorsByRangeEndpointTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>, IAsyncDisposable
 {
     private const string TestKey = "indicator:selic";
-    private readonly List<long> _insertedTimestamps = [];
 
     [Fact]
     public async Task GetIndicatorsByRange_ShouldReturnOk_WhenDataExists()
     {
-        await SeedTestDataAsync();
+        await IndicatorSeeder.SeedTestDataAsync(factory.Services, TestKey);
 
         DateOnly from = DateOnly.FromDateTime(DateTime.Now.AddDays(-10));
         DateOnly to = DateOnly.FromDateTime(DateTime.Now);
@@ -58,60 +57,11 @@ public class GetIndicatorsByRangeEndpointTests(WebApplicationFactory<Program> fa
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
-    private async Task SeedTestDataAsync()
-    {
-        using var scope = factory.Services.CreateScope();
-        IConnectionMultiplexer redis = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
-        IDatabase db = redis.GetDatabase();
-
-        if (await db.KeyExistsAsync(TestKey) == false)
-        {
-            await db.ExecuteAsync("TS.CREATE", TestKey, "RETENTION", 0, "LABELS", "code", "selic");
-        }
-
-        List<(long timestamp, decimal value)> testData = GenerateTestData(15);
-
-        List<object> args = [];
-
-        foreach (var (timestamp, value) in testData)
-        {
-            args.Add(TestKey);
-            args.Add(timestamp);
-            args.Add(value.ToString(CultureInfo.InvariantCulture));
-            _insertedTimestamps.Add(timestamp);
-        }
-
-        if (args.Count > 0)
-        {
-            await db.ExecuteAsync("TS.MADD", args.ToArray());
-        }
-    }
-
-    private static List<(long timestamp, decimal value)> GenerateTestData(int days)
-    {
-        List<(long, decimal)> data = [];
-        Random random = new(42);
-
-        for (int i = days; i >= 0; i--)
-        {
-            DateTime date = DateTime.Now.AddDays(-i).Date;
-            long timestamp = new DateTimeOffset(date).ToUnixTimeMilliseconds();
-            decimal value = 10.0m + (decimal)(random.NextDouble() * 3.0);
-
-            data.Add((timestamp, Math.Round(value, 2)));
-        }
-
-        return data;
-    }
-
     public async ValueTask DisposeAsync()
     {
-        if (_insertedTimestamps.Count > 0)
-        {
-            using var scope = factory.Services.CreateScope();
-            var redis = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
-            var db = redis.GetDatabase();
-            await db.KeyDeleteAsync(TestKey);
-        }
+        using var scope = factory.Services.CreateScope();
+        var redis = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
+        var db = redis.GetDatabase();
+        await db.KeyDeleteAsync(TestKey);
     }
 }
---
+++ tests/Iris.WebApi.FunctionalTests/Modules/Indicators/Seeders/IndicatorSeeder.cs
@@ -0,0 +1,55 @@
+using System.Globalization;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using StackExchange.Redis;
+
+namespace Iris.WebApi.FunctionalTests.Modules.Indicators.Seeders;
+
+public static class IndicatorSeeder
+{
+    public static async Task SeedTestDataAsync(IServiceProvider serviceProvider, string testKey)
+    {
+        using var scope = serviceProvider.CreateScope();
+        IConnectionMultiplexer redis = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
+        IDatabase db = redis.GetDatabase();
+
+        if (await db.KeyExistsAsync(testKey) == false)
+        {
+            await db.ExecuteAsync("TS.CREATE", testKey, "RETENTION", 0, "LABELS", "code", "selic");
+        }
+
+        List<(long timestamp, decimal value)> testData = GenerateTestData(15);
+
+        List<object> args = [];
+
+        foreach (var (timestamp, value) in testData)
+        {
+            args.Add(testKey);
+            args.Add(timestamp);
+            args.Add(value.ToString(CultureInfo.InvariantCulture));
+        }
+
+        if (args.Count > 0)
+        {
+            await db.ExecuteAsync("TS.MADD", args.ToArray());
+        }
+    }
+
+    private static List<(long timestamp, decimal value)> GenerateTestData(int days)
+    {
+        List<(long, decimal)> data = [];
+        Random random = new(42);
+
+        for (int i = days; i >= 0; i--)
+        {
+            DateTime date = DateTime.Now.AddDays(-i).Date;
+            long timestamp = new DateTimeOffset(date).ToUnixTimeMilliseconds();
+            decimal value = 10.0m + (decimal)(random.NextDouble() * 3.0);
+
+            data.Add((timestamp, Math.Round(value, 2)));
+        }
+
+        return data;
+    }
+}
